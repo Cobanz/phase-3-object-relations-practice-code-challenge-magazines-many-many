a748f9527e845001e7c29a4f307b30b9f2e5d35d
Charles Grant Nichols <grant@inkdots.com>
2021-05-26 12:50:24 -0500

    finished

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,22 @@
 class Reader < ActiveRecord::Base
+    has_many :subscription
+    has_many :magazine, through: :subscription
+
+    def subscribe(magazine, price)
+        Subscription.create(magazine: magazine, price: price, reader: self)
+    end
+
+    def total_subcription_price
+        total = self.subscription.map do |sub|
+            sub.price
+        end
+    total.sum
+    end
+
+    def cancel_subscription(magazine)
+        if self.magazine.inclue?(magazine)
+           sub = self.subscription.where("magazine_id = #{magazine.id}").first
+           sub.destory
+    end
   
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,10 @@
 class Subscription < ActiveRecord::Base
+    belongs_to :reader
+    belongs_to :magazine
+
+    def print_details
+        # {reader name} subscribed to {magazine title} for ${subscription price}
+        puts "#{self.reader.name} subscribed to #{self.magazine.title} for $#{self.price}"
+    end
   
 end
